b208d4cc918a1a0e60bb33a687cb977b29d04ebd
Shuolin Hu <hu.shuo@northeastern.edu>
2023-12-03 17:16:43 -0500

    assignment-10

--- lambda_function.js
+++ lambda_function.js
@@ -9,126 +9,117 @@ exports.handler = async (event) => {
     const dynamoDb = new AWS.DynamoDB.DocumentClient();
     let submissionId = uuidv4(); // Generate a unique submission ID
 
-    try {
-        // SNS message is in event.Records[0].Sns.Message
-        const snsMessage = JSON.parse(event.Records[0].Sns.Message);
-        const submissionUrl = snsMessage.submission_url;
-        const recipientEmail = snsMessage.email;
+    // SNS message is in event.Records[0].Sns.Message
+    const snsMessage = JSON.parse(event.Records[0].Sns.Message);
+    console.log("SNS Messages: ", snsMessage)
+    const submissionUrl = snsMessage.submission_url;
+    const recipientEmail = snsMessage.email;
 
-        if (!submissionUrl || !recipientEmail) {
-            throw new Error('Submission URL or recipient Email not provided in the SNS message');
-        }
+    // Environment variables for GCS and Mailgun
+    const gcsBucketName = process.env.GCS_BUCKET_NAME;
+    const gcpServiceAccountKeyJson = process.env.GCP_SERVICE_ACCOUNT_KEY_JSON;
+    const mailgunApiKey = process.env.MAILGUN_API_KEY;
+    const mailgunDomain = process.env.MAILGUN_DOMAIN;
+    const emailDeliveryTableName = process.env.EMAIL_DELIVERY_TABLE_NAME;
+    const snsStatus = snsMessage.status;
+    const logMessage = snsMessage.log_message;
+    const assignmentId = snsMessage.assignment_id;
 
-        // Environment variables for GCS and Mailgun
-        const gcsBucketName = process.env.GCS_BUCKET_NAME;
-        const gcpServiceAccountKeyJson = process.env.GCP_SERVICE_ACCOUNT_KEY_JSON;
-        const mailgunApiKey = process.env.MAILGUN_API_KEY;
-        const mailgunDomain = process.env.MAILGUN_DOMAIN;
-        const emailDeliveryTableName = process.env.EMAIL_DELIVERY_TABLE_NAME;
-        const snsStatus = snsMessage.status;
-        console.log('sns', snsStatus)
-        console.log('type of sns', typeof snsStatus)
-        const logMessage = snsMessage.log_message;
-        const assignmentId = snsMessage.assignment_id;
+    // Initialize Google Cloud Storage client
+    const storage = new Storage({
+        credentials: JSON.parse(gcpServiceAccountKeyJson)
+    });
+    const bucket = storage.bucket(gcsBucketName);
 
-        // Initialize Google Cloud Storage client
-        const storage = new Storage({
-            credentials: JSON.parse(gcpServiceAccountKeyJson)
-        });
-        const bucket = storage.bucket(gcsBucketName);
+    // Initialize Mailgun client
+    const mg = mailgun({ apiKey: mailgunApiKey, domain: mailgunDomain });
 
-        // Initialize Mailgun client
-        const mg = mailgun({ apiKey: mailgunApiKey, domain: mailgunDomain });
-
-        // Function to send email
-        const sendEmail = (subject, text) => {
-            const data = {
-                from: 'Shuolin <mailgun@demo.shuolin.me>',
-                to: recipientEmail,
-                subject: subject,
-                text: text,
-            };
-            return mg.messages().send(data);
+    // Function to send email
+    const sendEmail = (subject, text) => {
+        const data = {
+            from: 'Shuolin <mailgun@demo.shuolin.me>',
+            to: recipientEmail,
+            subject: subject,
+            text: text,
         };
+        return mg.messages().send(data);
+    };
 
-        // Function to record email in dynanoDB
-        async function recordEmailStatus(submissionId, email, status, errorMessage = '') {
-            const params = {
-                TableName: emailDeliveryTableName,
-                Item: {
-                    submissionId: submissionId,
-                    email: email,
-                    status: status,
-                    errorMessage: errorMessage,
-                    timestamp: new Date().toISOString()
-                }
-            };
-            try {
-                await dynamoDb.put(params).promise();
-                console.log(`Recorded email status for submissionId: ${submissionId}`);
-            } catch (error) {
-                console.error(`Error recording email status for submissionId: ${submissionId}`, error);
-
+    // Function to record email in dynamoDB
+    async function recordEmailStatus(submissionId, email, status, errorMessage = '') {
+        const params = {
+            TableName: emailDeliveryTableName,
+            Item: {
+                submissionId: submissionId,
+                email: email,
+                status: status,
+                errorMessage: errorMessage,
+                timestamp: new Date().toISOString()
             }
-        }        
+        };
+        try {
+            await dynamoDb.put(params).promise();
+            console.log(`Recorded email status for submissionId: ${submissionId}`);
+        } catch (error) {
+            console.error(`Error recording email status for submissionId: ${submissionId}`, error);
 
-        // Download the file from the URL and upload to GCS
-        const response = await axios.get(submissionUrl, { responseType: 'stream' });
-        const fileName = submissionUrl.split('/').pop();
-        const file = bucket.file(fileName);
-
-        await new Promise((resolve, reject) => {
-            const passthroughStream = new stream.PassThrough();
-            stream.pipeline(
-                response.data,
-                passthroughStream,
-                (error) => {
-                    if (error) {
-                        console.error('Error in stream pipeline', error);
-                        reject(error);
-                    }
-                }
-            );
-            passthroughStream.pipe(file.createWriteStream())
-                .on('error', reject)
-                .on('finish', resolve);
-        });
-
-        // Send success email
-        // await sendEmail('CSYE6225 Assignment Submission ${snsStatus}', `Assignment ${fileName} uploaded to ${gcsBucketName}`);
-        // console.log(`Uploaded ${fileName} to ${gcsBucketName}`);
-
-        // await sendEmail(`CSYE6225 Assignment Submission ${snsStatus}`, `${logMessage}, the Submission URL is ${submissionUrl}, the GCS path is ${gcsBucketName}/${fileName}`);
-
-       //  Send success email
-        const successSubject = 'CSYE6225 Assignment Submission Successful'
-        const failSubject = 'CSYE6225 Assignment Submission Failed'
-        if (snsStatus == 'Successful') {
-            await sendEmail(successSubject, `You submit assignment successfully, submission for assignment ${assignmentId}, it is uploaded to GCS path ${gcsBucketName}/${fileName}`);
-            console.log(`Uploaded ${fileName} to ${gcsBucketName}`);
-        } else {
-            await sendEmail(failSubject, `You submit assignment failed, the failed reason is `);
         }
+    }        
 
-        if (!submissionUrl.endsWith('.zip')) {
-            await sendEmail(failSubject, `You submit assignment failed, the failed reason is invalid url`);
-        }
 
-        // Record sucess email
-        await recordEmailStatus(submissionId, recipientEmail, 'Success');
-        console.log(`Success Email ${recipientEmail} recored to DynamoDB`);
-
-        return { statusCode: 200, body: `Uploaded ${fileName} to ${gcsBucketName}` };
-    } catch (error) {
-        console.error('Error:', error);
-
-        // Send failure email
-        await sendEmail('CSYE6225 Assignment Submission Failed', `Failed to upload file. Error: ${error.message}`);
-
-        // Record failure email
-        await recordEmailStatus(submissionId, recipientEmail, 'Failed', error.message);
-        console.log(`Failed Email ${recipientEmail} recored to DynamoDB`);
-
-        return { statusCode: 400, body: `Error: ${error.message}` };
+    // Send Failure Emails
+    const failSubject = 'CSYE6225 Assignment Submission Failed';
+    // Case 1: Submission deadline has passed
+    // Case 2: Exceeded number of attempts
+    if (snsStatus == 'Failed') {
+        // Send email
+        await sendEmail(failSubject, `You failed to submit your assignmnet ${assignmentId}, ${logMessage}`);
+        // Record email
+        await recordEmailStatus(submissionId, recipientEmail, snsStatus, logMessage);
+        return;
     }
+
+    // Case 3: Invalid submission URL
+    if (!submissionUrl.endsWith('.zip')) {
+        // Send email
+        await sendEmail(failSubject, `You failed to submit your assignment ${assignmentId}, the submission URL is invalid. A valid URL should point to a zip file`);
+        // Record email
+        await recordEmailStatus(submissionId, recipientEmail, 'Failed', "Invalid submission URL");
+        return;
+    }
+
+    // Download the file from the URL
+    const response = await axios.get(submissionUrl, { responseType: 'stream' });
+    const fileName = submissionUrl.split('/').pop();
+ 
+    // Define the path for the file in GCS
+    const gcsFilePath = `${recipientEmail}/${assignmentId}/${submissionId}/${fileName}`;
+    const file = bucket.file(gcsFilePath);
+
+    // Upload the downloaded file to GCS
+    await new Promise((resolve, reject) => {
+        const passthroughStream = new stream.PassThrough();
+        stream.pipeline(
+            response.data,
+            passthroughStream,
+            (error) => {
+                if (error) {
+                    console.error('Error in stream pipeline', error);
+                    reject(error);
+                }
+            }
+        );
+        passthroughStream.pipe(file.createWriteStream())
+            .on('error', reject)
+            .on('finish', resolve);
+    });
+
+    //  Send success email and record to dynamoDB
+    const successSubject = 'CSYE6225 Assignment Submission Successful'
+    if (snsStatus == 'Successful') {
+        await sendEmail(successSubject, `You successfully submitted the assignment "${assignmentId}", it is uploaded to the google cloud storage path ${gcsBucketName}/${gcsFilePath}`);
+        await recordEmailStatus(submissionId, recipientEmail, snsStatus);
+        return;
+    } 
+
 };
